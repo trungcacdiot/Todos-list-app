2fc3396685ee273d94e04bc8ac63551c8b9dd16a
trungbui <buibatrung01@gmail.com>
2020-08-22 09:23:22 +0700

    Updates

---
+++ precache-manifest.7ac70fc0c21149c9e86ad51ef58b13d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c82062692509e74f39aecf4facc465",
+    "url": "/Todos-list-app/index.html"
+  },
+  {
+    "revision": "5310d4786b251f1d949b",
+    "url": "/Todos-list-app/static/css/main.f7a917fc.chunk.css"
+  },
+  {
+    "revision": "6eae2a3447e8bec3fa10",
+    "url": "/Todos-list-app/static/js/2.9113aada.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Todos-list-app/static/js/2.9113aada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5310d4786b251f1d949b",
+    "url": "/Todos-list-app/static/js/main.e82ca1e7.chunk.js"
+  },
+  {
+    "revision": "7531a636bacebaf0c064",
+    "url": "/Todos-list-app/static/js/runtime-main.86e42b80.js"
+  },
+  {
+    "revision": "1466281417e7844c31ca6e2e86f76f67",
+    "url": "/Todos-list-app/static/media/check-ok.14662814.png"
+  },
+  {
+    "revision": "1716228755b4bb2ae47a91d644e48584",
+    "url": "/Todos-list-app/static/media/check.17162287.png"
+  }
+]);
